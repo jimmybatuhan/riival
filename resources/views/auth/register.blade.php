f54fa7aabb9d844fca1916199f00cf9e560bb3cb
Jimmy Batuhan <jimmybatuhan@gmail.com>
2020-11-05 17:52:56 +0800

    auth and registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,64 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-	<!-- Page Content -->
-    <div class="bg-pattern-style bg-pattern-style-register">
-				<div class="content">
-
-					<!-- Register Content -->
-					<div class="account-content">
-						<div class="account-box">
-							<div class="login-right">
-								<div class="login-header">
-									<h3><span>Mentoring</span> Register</h3>
-									<p class="text-muted">Access to our dashboard</p>
-								</div>
-
-								<!-- Register Form -->
-								<form action="index">
-									<div class="row">
-										<div class="col-lg-6">
-											<div class="form-group">
-												<label class="form-control-label">First Name</label>
-												<input id="first-name" type="text" class="form-control" name="first_name" autofocus="">
-											</div>
-										</div>
-										<div class="col-lg-6">
-											<div class="form-group">
-												<label class="form-control-label">Last Name</label>
-												<input id="last-name" type="text" class="form-control" name="last_name">
-											</div>
-										</div>
-									</div>
-									<div class="form-group">
-										<label class="form-control-label">Email Address</label>
-										<input id="email" type="email" class="form-control">
-									</div>
-                                    <div class="form-group">
-                                        <label class="form-control-label">Password</label>
-                                        <input id="password" type="password" class="form-control" name="password">
-                                    </div>
-									<div class="form-group">
-										<div class="custom-control custom-control-xs custom-checkbox">
-											<input type="checkbox" class="custom-control-input" name="agreeCheckboxUser" id="agree_checkbox_user">
-											<label class="custom-control-label" for="agree_checkbox_user">I agree to the</label> <a tabindex="-1" href="javascript:void(0);">Privacy Policy</a> &amp; <a tabindex="-1" href="javascript:void(0);"> Terms.</a>
-										</div>
-									</div>
-									<button class="btn btn-primary login-btn" type="submit">Create Account</button>
-									<div class="account-footer text-center mt-3">
-										Already have an account? <a class="forgot-link mb-0" href="login">Login</a>
-									</div>
-								</form>
-								<!-- /Register Form -->
-
-							</div>
-						</div>
-					</div>
-					<!-- /Register Content -->
-
-				</div>
-
-			</div>
-			<!-- /Page Content -->
-@
+<div class="bg-pattern-style bg-pattern-style-register">
+    <div class="content">
+        <div class="account-content">
+            <div class="account-box">
+                <div class="login-right">
+                    <div class="login-header">
+                        <h3><span>Mentoring</span> Register</h3>
+                        <p class="text-muted">Access to our dashboard</p>
+                    </div>
+                    <form action="{{ route('register') }}" method="POST">
+                        @csrf
+                        @if($errors->any())
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {!! implode('', $errors->all('<div>:message</div>')) !!}
+                            </div>
+                        @endif
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label class="form-control-label">First Name</label>
+                                    <input
+                                        autofocus=""
+                                        class="form-control"
+                                        id="first-name"
+                                        name="first_name"
+                                        type="text"
+                                        required
+                                        value="{{ old('first_name') }}"
+                                    >
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label class="form-control-label">Last Name</label>
+                                    <input
+                                        class="form-control"
+                                        id="last-name"
+                                        name="last_name"
+                                        type="text"
+                                        required
+                                        value="{{ old('last_name') }}"
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label">Email Address</label>
+                            <input id="email" type="email" class="form-control" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label">Password</label>
+                            <input id="password" type="password" class="form-control" name="password" required>
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-control-xs custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="read_and_agreed_terms_and_conditions"
+                                    id="agree_checkbox_user">
+                                <label class="custom-control-label" for="agree_checkbox_user">I agree to the</label> <a
+                                    tabindex="-1" href="javascript:void(0);">Privacy Policy</a> &amp; <a tabindex="-1"
+                                    href="javascript:void(0);"> Terms.</a>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary login-btn" type="submit">Create Account</button>
+                        <div class="account-footer text-center mt-3">
+                            Already have an account? <a class="forgot-link mb-0" href="login">Login</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,14 @@
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
 
+
+                        @endif
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="submit" />Logout</button>
+                        </form>
                     {{ __('You are logged in!') }}
                 </div>
             </div>
