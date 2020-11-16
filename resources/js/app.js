0b0aab069847dfc1080c5a1f32b0188f069c4ec6
Jimmy Batuhan <jimmybatuhan@gmail.com>
2020-11-16 09:46:41 +0800

    added content

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,34 @@
 "use strict";
 
-require('./bootstrap');
-
 import Vue from "vue";
 import Vuex from "vuex";
+// import Echo from 'laravel-echo';
+
+import axios from "axios";
 
 import router from "src/config/router";
 import App from "src/modules/App";
 
 import global from "src/mixins/global";
 
+// window.Pusher = require('pusher-js');
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true
+// });
+
 window.Vue = Vue;
 
 Vue.use(Vuex);
-
 Vue.mixin(global);
 
-Vue.prototype.$csrf = document.head.querySelector('meta[name="csrf-token"]').content;
+let token = document.head.querySelector('meta[name="csrf-token"]');
+axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
+
+Vue.prototype.$axios = axios;
+Vue.prototype.$csrf = token.content;
 
 const app = new Vue({
     router,
